37accac1f20854678de49a0c41ac60171b107a9d
Drăghici Andrei <35525357+Sekushy@users.noreply.github.com>
2022-05-19 07:41:05 +0300

    Added constants to the Constants file

--- src/test/java/Constants.java
+++ src/test/java/Constants.java
@@ -1,3 +1,5 @@
 public class Constants {
     // This is a class where you will keep all of your test related constants.
+    final static String CHROME_DRIVER_LOCATION = "chromedriver.exe";
+    final static String BASE_URL = "";
 }
